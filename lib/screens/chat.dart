5169edfbcf350759ea92d7952906d6cc62892cc2
admin <william0129@gmail.com>
2024-04-15 15:26:42 +0800

    enable user logout

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatScreen extends StatelessWidget{
   const ChatScreen ({super.key});
@@ -8,6 +9,15 @@ class ChatScreen extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         title: const Text ('Flutterchat'),
+        actions: [
+          IconButton(
+            onPressed: (){
+              FirebaseAuth.instance.signOut();
+            }, 
+            icon: Icon(Icons.logout,
+              color: Theme.of(context).colorScheme.primary),
+            )
+        ]
       ),
       body: const Center(
         child: Text('Logged in!')
